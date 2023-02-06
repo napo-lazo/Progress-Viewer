10e05e79e5107ac141d849d2c959700e05b79014
Jose Napoleon Lazo Celaya <napoleon.lazo@hotmail.com>
2023-02-05 20:20:50 -0600

    Load the progress entries file into a dictionary

--- File_Manager.py
+++ File_Manager.py
@@ -1,8 +1,22 @@
 import os
+from json import load, dumps
 
 class File_Manager():
 
     def CheckForFolder(self) -> None:
-        '''Checks if the folder holding our files exists where the script was run from, if not creates a new one'''
+        '''Checks if the folder that holds the required files exists where the script was run from, if not creates a new one'''
         if (not os.path.exists("./Progress_Viewer_Data")):
-            os.mkdir('./Progress_Viewer_Data')
+            os.mkdir("./Progress_Viewer_Data")
+            
+    def LoadProgressEntries(self) -> dict:
+        '''Checks if there the file holding our progress entries exists and loads it into a dictionary, else it creates a new file and dictionary'''
+        if (not os.path.exists("./Progress_Viewer_Data/Progress_Entries.json")):
+            baseJsonDictionary = {"Entries": []}
+            with open("./Progress_Viewer_Data/Progress_Entries.json", "x") as jsonFile:
+                serializedDictionary = dumps(baseJsonDictionary, indent=2)
+                jsonFile.write(serializedDictionary)
+
+            return baseJsonDictionary
+        else:
+            with open("./Progress_Viewer_Data/Progress_Entries.json", "r") as jsonFile:
+                return load(jsonFile)
--- Progress_Viewer.py
+++ Progress_Viewer.py
@@ -1,11 +1,11 @@
-from File_Manager import File_Manager
+from Entry_Manager import Entry_Manager
 from Dependencies.Menu_Plotter.Menu_Plotter import Menu_Plotter
 
 def GenerateMenuStructure() -> Menu_Plotter:
     menu = Menu_Plotter()
-    fileManager = File_Manager()
+    entryManager = Entry_Manager()
 
-    menu.AddActionNode("Initial Check", ["Main Screen"], fileManager.CheckForFolder)
+    menu.AddActionNode("Initial Check", ["Main Screen"], entryManager.InitialCheck)
     menu.AddMenuNode("Main Screen", ["exit"], ["Exit"])
     menu.SetStartNode("Initial Check")
 
